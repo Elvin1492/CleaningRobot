9be8d04ff0f1a16381bfd3da18ddf1182c2eaa25
Elvin <elvin.arzumanoglu@gmail.com>
2018-05-11 20:51:52 +0300

    start points for clean service

---
+++ CleaningRobot.Infrastructure/CleanService.cs
@@ -0,0 +1,19 @@
+﻿using CleaningRobot.Infrastructure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleaningRobot.Infrastructure
+{
+    public class CleanService
+    {
+        private readonly Order _order;
+
+        public CleanService(Order order)
+        {
+            this._order = order;
+        }
+    }
+}
